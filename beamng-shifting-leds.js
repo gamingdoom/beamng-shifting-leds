2120b955264b859de765f2242d43d1ccc58b37c8
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2022-08-31 19:02:35 -0700

    adjust ratios, add shebang, allow red lights and flashing to be independent

--- beamng-shifting-leds.js
+++ beamng-shifting-leds.js
@@ -1,8 +1,11 @@
+#!/usr/bin/node
+
 // Using some code from https://github.com/d4rk/ac_shifting_leds/blob/main/ac_shifting_leds.js and https://github.com/fuelsoft/out-gauge-cluster/blob/master/OutGaugeCluster/OutGaugeCluster.pde
 
 const hid = require('node-hid');
 const dgram = require('dgram');
 const { abort, exit } = require('process');
+const {setTimeout} = require('timers/promises');
 
 const socket = dgram.createSocket('udp4');
 const PORT = 5555;
@@ -12,6 +15,7 @@ var rpm;
 var maxrpm = 7000;
 var g29;
 var firstTime = true;
+var LEDFlashTime = 0;
 
 process.stdin.on('data', data => {
   maxrpm = Number(data.toString());
@@ -46,37 +50,36 @@ socket.on("message", (msg, rinfo) => {
 function updateLeds(){
     const rpmFrac = rpm / maxrpm;
 
+    this.hrTime = process.hrtime();
+
     // Convert rpmFrac to an LED range.
     var LEDMask = 0x0;
-    if (rpmFrac > 0.4) {
+    if (rpmFrac > 0.45) {
         LEDMask |= 0x1;
     }
-    if (rpmFrac > 0.5) {
+    if (rpmFrac > 0.55) {
         LEDMask |= 0x2;
     }
-    if (rpmFrac > 0.6) {
+    if (rpmFrac > 0.625) {
         LEDMask |= 0x4;
     }
-    if (rpmFrac > 0.7) {
+    if (rpmFrac > 0.71) {
         LEDMask |= 0x8;
     }
     if (rpmFrac > 0.85) {
         LEDMask |= 0x10;
     }
+    // Flash LEDs
+    if (rpmFrac > 0.93){
+        if (this.hrTime[0] * 1000000 + this.hrTime[1] / 1000 - LEDFlashTime > 100000){
+            flashLEDs();
+        }
+    }
     if (LEDMask == this.previousLEDMask) {
         return;
     }
     this.previousLEDMask = LEDMask;
-    // If we're max-ed out i.e. probably redline, then flash all the LEDs.
-    if (LEDMask == 0x1f) {
-        this.flashLEDsTimer = setInterval(function () {flashLEDs(); }, 100);
-    } else {
-        if (this.flashLEDsTimer) {
-        clearInterval(this.flashLEDsTimer);
-        this.flashLEDsTimer = undefined;
-        }
-        g29.write([0xf8, 0x12, LEDMask, 0x00, 0x00, 0x00, 0x01])
-    }
+    g29.write([0xf8, 0x12, LEDMask, 0x00, 0x00, 0x00, 0x01])
 }
 
 function flashLEDs() {
@@ -86,4 +89,5 @@ function flashLEDs() {
       g29.write([0xf8, 0x12, 0, 0x00, 0x00, 0x00, 0x01])
     }
     this.LEDsOn = !this.LEDsOn;
-  }
+    LEDFlashTime = this.hrTime[0] * 1000000 + this.hrTime[1] / 1000;
+}
